a6341a122313705a415c8960ad62c446fe4fa349
Amine Yaakoubi <45365762+DevAly@users.noreply.github.com>
2024-03-26 21:53:25 +0000

    increase time left and optimize code

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 let currentQuestionIndex = 0;
 let score = 0;
 let questions = [];
-let userResponses = {}; // Array to store user responses
-let timeLeft = 60; // Set timer duration in seconds
+let userResponses = {};
+let timeLeft = 300;
 document.addEventListener('DOMContentLoaded', function () {
     const questionContainer = document.getElementById('question-container');
     const submitButton = document.getElementById('submit-btn');
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     showResults();
                 }
             }
-        }, 1000); // Update timer every second
+        }, 1000);
     }
 
     function updateProgressBar() {
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let value = typeof userResponses[currentQuestionIndex] !== "undefined" ? userResponses[currentQuestionIndex] : '';
 
         if (typeof currentQuestion.answer === 'string') {
-            // Write it yourself question type
+         
             questionContainer.innerHTML = `
                         <h2>${currentQuestion.question}</h2>
                         <input type="text" id="write-answer" value="${value}">
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             let checkedValues = typeof userResponses[currentQuestionIndex] !== "undefined" ? userResponses[currentQuestionIndex] : [];
 
-            // Multiple-choice question type
+            
             const optionsHtml = currentQuestion.options.map((option, index) => {
                 if (Array.isArray(currentQuestion.answer)) {
                     return `
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const currentQuestion = questions[currentQuestionIndex];
         feedbackContainer.textContent = '';
         if (typeof currentQuestion.answer === 'string') {
-            // Write it yourself question type
+            
             const writeAnswerInput = document.getElementById('write-answer');
             const userAnswer = writeAnswerInput.value.trim().toLowerCase();
             if (userAnswer == '') {
@@ -113,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             saveUserResponse(userAnswer, currentQuestionIndex); // Save user response
         } else {
-            // Multiple-choice question type
+           
             const selectedOptions = document.querySelectorAll('input[name="option"]:checked');
             if (selectedOptions.length === 0) {
                 feedbackContainer.textContent = 'Veuillez sélectionner au moins une option.';
@@ -137,12 +137,11 @@ document.addEventListener('DOMContentLoaded', function () {
         questionContainer.innerHTML = '';
         submitButton.style.display = 'none';
         let resultHTML = '';
-        let score = 0; // Initialize score variable
+        let score = 0; 
         resultHTML += '<ul>';
         for (index = 0; index < questions.length; index++) {
             var question = questions[index];
-            // }
-            // questions.forEach((question, index) => {
+            
             resultHTML += '<li>';
             resultHTML += `<h2>Question ${index + 1}: ${question.question}</h2>`;
             resultHTML += '<p><strong>Votre réponse:</strong>';
@@ -160,9 +159,9 @@ document.addEventListener('DOMContentLoaded', function () {
             else {
                 if (typeof question.answer === 'string') {
                     resultHTML += escapeHtml(userResponses[index]);
-                    // Check if user response matches correct answer for string type questions
+                  
                     if (userResponses[index] === question.answer) {
-                        score++; // Increment score if correct
+                        score++; 
                     } else {
                         resultHTML += '</p>';
                         resultHTML += '<p><i>Réponse correcte:';
@@ -176,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     const userResponseText = userResponses[index].map(responseIndex => question.options[responseIndex]).join(', ');
                     resultHTML += userResponseText;
                     if (arraysEqual(sortedUserResponse, sortedCorrectAnswer)) {
-                        score++; // Increment score if correct
+                        score++;
                     } else {
 
                         resultHTML += '</p>';
@@ -186,9 +185,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 } else {
                     resultHTML += escapeHtml(question.options[userResponses[index]]);
-                    // Check if user response matches correct answer for single choice questions
+                   
                     if (userResponses[index] == question.answer) {
-                        score++; // Increment score if correct
+                        score++; 
                     } else {
                         resultHTML += '</p>';
                         resultHTML += '<p><i>Réponse correcte:';
@@ -203,13 +202,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
         resultHTML = `<p>Vous avez obtenu ${score} sur ${questions.length} questions.</p>` + resultHTML;
         feedbackContainer.innerHTML = resultHTML;
-        console.log("User Responses:", userResponses); // Print user responses
+        console.log("User Responses:", userResponses); 
 
 
 
     }
 
-    // Function to check if two arrays are equal
     function arraysEqual(arr1, arr2) {
         if (arr1.length !== arr2.length) return false;
         for (let i = 0; i < arr1.length; i++) {
@@ -227,8 +225,6 @@ document.addEventListener('DOMContentLoaded', function () {
         return array;
     }
 
-
-    // Fetch questions from JSON file
     fetch('questions.json')
         .then(response => response.json())
         .then(data => {
